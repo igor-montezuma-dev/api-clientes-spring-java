0fafeb5252882513fcbbb49823222ad80e37c0b4
IgorMontezuma20 <igorminranda17@gmail.com>
2023-08-21 15:24:00 -0300

    feat: criando a rota de criacao de Clientes

--- src/main/java/io/github/igormontezuma20/clientes/ClientesApplication.java
+++ src/main/java/io/github/igormontezuma20/clientes/ClientesApplication.java
@@ -1,10 +1,23 @@
 package io.github.igormontezuma20.clientes;
 
+import io.github.igormontezuma20.clientes.model.entity.Cliente;
+import io.github.igormontezuma20.clientes.model.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ClientesApplication {
+
+	@Bean
+	public CommandLineRunner run(@Autowired ClienteRepository repository){
+		return args -> {
+			Cliente cliente = new Cliente().builder().cpf("00000000000").nome("Igor").build();
+			repository.save(cliente);
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientesApplication.class, args);
---
+++ src/main/java/io/github/igormontezuma20/clientes/rest/ClienteController.java
@@ -0,0 +1,27 @@
+package io.github.igormontezuma20.clientes.rest;
+
+import io.github.igormontezuma20.clientes.model.entity.Cliente;
+import io.github.igormontezuma20.clientes.model.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/clientes")
+public class ClienteController {
+
+    private final ClienteRepository repository;
+    @Autowired
+    public ClienteController(ClienteRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente salvar(Cliente cliente){
+        return repository.save(cliente);
+    }
+}
