ecebb0cc5d3377457db48a1205ed3b9d88862613
IgorMontezuma20 <igorminranda17@gmail.com>
2023-08-21 15:55:17 -0300

    feat: testando cadastro de Cliente

--- src/main/java/io/github/igormontezuma20/clientes/model/entity/Cliente.java
+++ src/main/java/io/github/igormontezuma20/clientes/model/entity/Cliente.java
@@ -1,5 +1,6 @@
 package io.github.igormontezuma20.clientes.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -23,6 +24,7 @@ public class Cliente {
     @Column(nullable =false, length = 11)
     private String cpf;
     @Column(name = "data_cadastro")
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataCadastro;
 
     @PrePersist
