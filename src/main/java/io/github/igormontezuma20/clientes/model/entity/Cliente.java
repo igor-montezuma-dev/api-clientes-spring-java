d41c70e17aa50c37fc91f76cd7be9a7b8a6ee2a1
IgorMontezuma20 <igorminranda17@gmail.com>
2023-08-21 14:24:16 -0300

    feat: mapeando a entidade Cliente

---
+++ src/main/java/io/github/igormontezuma20/clientes/model/entity/Cliente.java
@@ -0,0 +1,19 @@
+package io.github.igormontezuma20.clientes.model.entity;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable =false, length = 150)
+    String nome;
+    @Column(nullable =false, length = 11)
+    private String cpf;
+    @Column()
+    private LocalDate dataCadastro;
+
+}
