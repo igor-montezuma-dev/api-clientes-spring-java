685f5448e0d172b7d868abbca93b36dcfb6eebf1
IgorMontezuma20 <igorminranda17@gmail.com>
2023-08-21 14:16:08 -0300

    feat: projeto inicializado

---
+++ src/test/java/io/github/igormontezuma20/clientes/ClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.igormontezuma20.clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
