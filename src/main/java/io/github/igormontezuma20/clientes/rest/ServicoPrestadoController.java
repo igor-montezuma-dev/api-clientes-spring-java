db9ac8406110669a9f716faff010d64fcfa07a8f
IgorMontezuma20 <igorminranda17@gmail.com>
2023-08-27 22:12:45 -0300

    feat: implementando criacao de ServicoPrestado

--- src/main/java/io/github/igormontezuma20/clientes/rest/ServicoPrestadoController.java
+++ src/main/java/io/github/igormontezuma20/clientes/rest/ServicoPrestadoController.java
@@ -5,13 +5,11 @@ import io.github.igormontezuma20.clientes.model.entity.ServicoPrestado;
 import io.github.igormontezuma20.clientes.model.repository.ClienteRepository;
 import io.github.igormontezuma20.clientes.model.repository.ServicoPrestadoRepository;
 import io.github.igormontezuma20.clientes.rest.dto.ServicoPrestadoDTO;
+import io.github.igormontezuma20.clientes.util.BigDecimalConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
@@ -25,8 +23,10 @@ public class ServicoPrestadoController {
 
     private final ClienteRepository clienteRepository;
     private final ServicoPrestadoRepository servicoPrestadoRepository;
+    private final BigDecimalConverter bigDecimalConverter;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ServicoPrestado createService(@RequestBody ServicoPrestadoDTO dto){
 
         LocalDate serviceDate = LocalDate.parse(dto.getData(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
@@ -40,9 +40,10 @@ public class ServicoPrestadoController {
 
         ServicoPrestado servicoPrestado = new ServicoPrestado();
         servicoPrestado.setDescricao(dto.getDescricao());
-        servicoPrestado.setValor(dto.getPreco());
+        servicoPrestado.setValor(bigDecimalConverter.converter(dto.getPreco()));
         servicoPrestado.setData(serviceDate);
         servicoPrestado.setCliente(cliente);
 
+        return servicoPrestadoRepository.save(servicoPrestado);
     }
 }
