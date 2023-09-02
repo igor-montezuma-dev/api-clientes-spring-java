add01ab6eaa87f9b323545b865248576f24e68ad
IgorMontezuma20 <igorminranda17@gmail.com>
2023-09-02 20:25:12 -0300

    ajuste

--- src/main/java/io/github/igormontezuma20/clientes/rest/UsuarioController.java
+++ src/main/java/io/github/igormontezuma20/clientes/rest/UsuarioController.java
@@ -12,7 +12,7 @@ import org.springframework.web.server.ResponseStatusException;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/api/usuarios")
+@RequestMapping(value = "/api/usuarios",  method=RequestMethod.POST)
 @RequiredArgsConstructor
 public class UsuarioController {
 
